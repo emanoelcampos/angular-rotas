7f3c3101f589fdce5c93ed609efeeba62d6981e1
emanoelcampos <emanoel.cv@gmail.com>
2022-10-18 01:46:22 -0300

    definindo e extraindo parâmetros de roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
 
 const routes: Routes = [
   {
@@ -12,7 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'curso/:id',
-    component: CursosComponent
+    component: CursoDetalheComponent
   },
   {
     path: 'login',
--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  constructor() { }
+  id: string = '';
+
+  constructor(private route: ActivatedRoute) {
+    this.id = this.route.snapshot.params['id'];
+  }
 
   ngOnInit(): void {
   }
