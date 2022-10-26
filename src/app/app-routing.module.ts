709a65e1520a084ca67bed810df0fc596f5e39d0
emanoelcampos <emanoel.cv@gmail.com>
2022-10-26 19:10:21 -0300

    rota padrao e wild card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
-import { AlunosGuard } from './guards/alunos.guard ';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
 const routes: Routes = [
@@ -30,9 +30,19 @@ const routes: Routes = [
     component: LoginComponent
   },
   {
-    path: '',
+    path: 'home',
     component: HomeComponent,
     canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PaginaNaoEncontradaComponent//,
+    //canActivate: [AuthGuard]
   }
 ];
 
